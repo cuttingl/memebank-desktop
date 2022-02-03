101c6296d26a334bb81a321db17f43daec9fa551
cuttingl <laurent.cutting@gmail.com>
2022-02-03 11:33:00 +0100

    first function to detect object in image

---
+++ main.py
@@ -0,0 +1,19 @@
+from cv2 import cv2
+
+# detect object in image
+# language : python
+def object_in_image(image):
+    # read image
+    img = cv2.imread(image)
+    # convert image to gray scale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # detect face
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(gray, 1.3)
+    # draw rectangle around face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+if __name__ == '__main__':
+    object_in_image('face.jpg')
+    
