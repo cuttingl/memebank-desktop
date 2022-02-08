08a23298796df48a15a2d74cd71858853129fd78
cuttingl <laurent.cutting@gmail.com>
2022-02-08 17:05:27 +0100

    object recognition algo added : not working

--- object.py
+++ object.py
@@ -1,10 +1,11 @@
 import numpy as np
 import os
-import six as urllib
+import six.moves.urllib as urllib
 import sys
 import tarfile
 import tensorflow as tf
 import zipfile
+import utils as utils
  
 from collections import defaultdict
 from io import StringIO
@@ -16,13 +17,9 @@ cap = cv2.VideoCapture(0)
  
 sys.path.append("..")
  
-from utils import label_map_util
- 
-from utils import visualization_utils as vis_util
- 
-MODEL_NAME = 'ssd_mobilenet_v1_1_metadata_1'
-MODEL_FILE = MODEL_NAME + '.tflite'
-DOWNLOAD_BASE = '<a href="http://download.tensorflow.org/models/object_detection/">http://download.tensorflow.org/models/object_detection/</a>'
+MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
  
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
@@ -31,6 +28,7 @@ PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
 PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
  
 NUM_CLASSES = 90
+IMAGE_SIZE = (12, 8)
  
 opener = urllib.request.URLopener()
 opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
@@ -48,9 +46,9 @@ with detection_graph.as_default():
     od_graph_def.ParseFromString(serialized_graph)
     tf.import_graph_def(od_graph_def, name='')
  
-label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-category_index = label_map_util.create_category_index(categories)
+label_map = utils.label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = utils.label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = utils.label_map_util.create_category_index(categories)
  
 with detection_graph.as_default():
   with tf.Session(graph=detection_graph) as sess:
@@ -71,7 +69,7 @@ with detection_graph.as_default():
       [boxes, scores, classes, num_detections],
       feed_dict={image_tensor: image_np_expanded})
     # Visualization of the results of a detection.
-      vis_util.visualize_boxes_and_labels_on_image_array(
+      utils.vis_util.visualize_boxes_and_labels_on_image_array(
           image_np,
           np.squeeze(boxes),
           np.squeeze(classes).astype(np.int32),
@@ -80,7 +78,8 @@ with detection_graph.as_default():
           use_normalized_coordinates=True,
           line_thickness=8)
   
+      # show final image
       cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
-      if cv2.waitKey(25) == ord('q'):
+      if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
